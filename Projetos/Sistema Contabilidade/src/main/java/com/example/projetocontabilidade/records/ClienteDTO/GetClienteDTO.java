514c717d6e5ed2e45621a879f3b7db51223f1265
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2025-07-04 18:01:46 -0300

    Delete Projetos/Sistema Contabilidade directory

--- Projetos/Sistema Contabilidade/src/main/java/com/example/projetocontabilidade/records/ClienteDTO/GetClienteDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.projetocontabilidade.records.ClienteDTO;
-
-import com.example.projetocontabilidade.model.Estado;
-
-public record GetClienteDTO (Long id, String nome, String cpf, String cidade, Estado estado){
-}
--- Projetos/Sistema Contabilidade/src/main/java/com/example/projetocontabilidade/repository/VendaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projetocontabilidade.repository;
-
-import com.example.projetocontabilidade.model.Venda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendaRepository extends JpaRepository<Venda, Long> {
-}
